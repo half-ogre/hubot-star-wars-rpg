ae3ffa874f00ea2b55800f5ab0c523fa199dd7ab
half-ogre <ego@half-ogre.com>
2015-08-08 09:55:16 -0700

    the guts (package, license, script loading)

---
+++ index.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(robot) {
+  var scriptsPath = path.resolve(__dirname, 'src');
+  fs.readdir(scriptsPath, function(err, files) {
+    if (err) {
+      throw err;
+    }
+
+    files
+      .sort()
+      .foreach(function(file) {
+        robot.loadFile(scriptsPath, file);
+      });
+  });
+};
